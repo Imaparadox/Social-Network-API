930764aac7c741d9eb0172ba1b8e0b5a203a170a
Imaparadox <stevemarsh117@gmail.com>
2021-08-14 08:33:50 -0400

    Got user get routes and post to work

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,29 +1,33 @@
-const { User, Thought } =require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
-    //Get all users
-    getAllUsers(req, res) {
-        User.find({})
-        .select('-__v')
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-          console.log(err);
-          res.sendStatus(400).json(err)});
-    },
-    //Get user by id
-    getUserById(req, res) {
-        User.findOne({ _id: params.id })
-        .populate({
-          path: 'thought',
-          select: '-__v'
-        })
-        .select('-__v')
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-          console.log(err);
-          res.sendStatus(400);
-        });
-    }
+  //Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400).json(err)
+      });
+  },
+  //Get user by id
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.id })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400).json(err);
+      });
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(500).json(err))
+  }
 }
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const {
     getAllUsers,
-    
+    getUserById,
+    createUser
 } = require('../../controllers/user-controller');
 
 // /api/users
 router
     .route('/')
     .get(getAllUsers)
+    .post(createUser)
+
+
+router.route('/:id')
+    .get(getUserById)
 
 module.exports = router;
